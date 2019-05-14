f78736cc6f2aa4079a1345e503bc830a1ee0a39b
Guillaume Berche <guillaume.berche@orange.com>
2019-05-14 14:39:38 +0200

    fix whitesource security scan priority
    Try to use multi module: dump generated setup file

--- whitesource-scan.sh
+++ whitesource-scan.sh
@@ -21,8 +21,12 @@ fi
 # https://whitesource.atlassian.net/wiki/spaces/WD/pages/651919363/EUA+Support+for+Multi-Module+Analysis
 #First discover submodules and save it into a properties file
 java -jar wss-unified-agent.jar -d $(pwd) -analyzeMultiModule whitesource-multi-module.properties
+
+echo "whitesource-multi-module.properties content:"
+cat whitesource-multi-module.properties
+
 #Then invoke analysis on all modules
-java -jar wss-unified-agent.jar -xModulePath whitesource-multi-module.properties -fsaJarPath wss-unified-agent.jar -c wss-unified-agent.config -statusDisplay dynamic -apiKey $WHITESOURCE_API_KEY
+java -jar wss-unified-agent.jar -xModulePath whitesource-multi-module.properties -fsaJarPath wss-unified-agent.jar -c whitesource_config.properties -statusDisplay dynamic -apiKey $WHITESOURCE_API_KEY
 
 
 for currentFolderName in $(ls -d1 cf-ops-automation*/)
